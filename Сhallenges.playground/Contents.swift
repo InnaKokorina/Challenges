dde690429fcd3251f725523a828108698a01e5b8
Inna Kokorina <inna.kokorina@rt.ru>
2022-09-08 11:33:10 +0400

    add task 14 Arcade

--- Сhallenges.playground/Contents.swift
+++ Сhallenges.playground/Contents.swift
@@ -364,43 +364,90 @@ import UIKit
 // 3.Interview Practice rotateImage
 //You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
 // первое решение O(15)
-func solution(a: [[Int]]) -> [[Int]] {
-    let n = a.flatMap { $0 }
-    let inputCount = a[0].count
-    var outputArray = [[Int]]()
-
-    for i in 0...inputCount - 1 {
-        var inoutArray =  [Int]()
-        for j in 0...inputCount-1 where i + j*inputCount <  n.count{
-            inoutArray.append(n[i + j*inputCount])
-        }
-        outputArray.append(inoutArray.reversed())
-    }
-   return outputArray
-}
-// второе решениие O(12)
-func solutionSecond(a: [[Int]]) -> [[Int]] {
-    var new = a
-    for i in 0..<a.count {
-        for j in 0..<a[i].count {
-            new[j][i] = a[i][j]
-        }
-    }
-    for i in 0..<new.count {
-        new[i] = new[i].reversed()
-    }
-   return new
-}
-
-solution(a: [[1, 2, 3],
-             [4, 5, 6],
-             [7, 8, 9]])
-
-solutionSecond(a: [[1, 2, 3],
-                   [4, 5, 6],
-                   [7, 8, 9]])
+//func solution(a: [[Int]]) -> [[Int]] {
+//    let n = a.flatMap { $0 }
+//    let inputCount = a[0].count
+//    var outputArray = [[Int]]()
+//
+//    for i in 0...inputCount - 1 {
+//        var inoutArray =  [Int]()
+//        for j in 0...inputCount-1 where i + j*inputCount <  n.count{
+//            inoutArray.append(n[i + j*inputCount])
+//        }
+//        outputArray.append(inoutArray.reversed())
+//    }
+//   return outputArray
+//}
+//// второе решениие O(12)
+//func solutionSecond(a: [[Int]]) -> [[Int]] {
+//    var new = a
+//    for i in 0..<a.count {
+//        for j in 0..<a[i].count {
+//            new[j][i] = a[i][j]
+//        }
+//    }
+//    for i in 0..<new.count {
+//        new[i] = new[i].reversed()
+//    }
+//   return new
+//}
+//
+//solution(a: [[1, 2, 3],
+//             [4, 5, 6],
+//             [7, 8, 9]])
+//
+//solutionSecond(a: [[1, 2, 3],
+//                   [4, 5, 6],
+//                   [7, 8, 9]])
 //solution(a) =
 //    [[7, 4, 1],
 //     [8, 5, 2],
 //     [9, 6, 3]]
+// ---------------------------------------
 
+//4.Arcade alternatingSums
+//Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+//
+//You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+
+
+// первое решение
+func solution(a: [Int]) -> [Int] {
+    var firstArray = [Int]()
+    var secondArray = [Int]()
+    for i in 0..<a.count {
+        if i % 2 == 0 {
+            firstArray.append(a[i])
+        } else {
+            secondArray.append(a[i])
+        }
+    }
+    return [firstArray.reduce(0, +), secondArray.reduce(0, +)]
+}
+// второе более красивое решение
+func solutionsSecond(a: [Int]) -> [Int] {
+    let firstArray = a.enumerated().filter { $0.0 % 2 == 0 }.map{ $0.1 }
+    let secondArray = a.enumerated().filter { $0.0 % 2 != 0 }.map{ $0.1 }
+    return [firstArray.reduce(0, +), secondArray.reduce(0, +)]
+}
+//третье более быстрое решение O(N)
+
+func solutionThird(a: [Int]) -> [Int] {
+    var result = [0,0]
+    for i in 0..<a.count {
+        if i % 2 == 0 {
+            result[0] += a[i]
+        } else {
+            result[1] += a[i]
+        }
+    }
+    return result
+}
+
+
+let a = [50, 60, 60, 45, 70]
+solution(a: a)
+solutionsSecond(a: a)
+solutionThird(a: a)
+
+//= [180, 105]
